095cd6d79357dbaf5e759df839bb79a03b1ebd8d
Mhmdzend22 <166401046+Mhmdzend22@users.noreply.github.com>
2024-07-26 14:17:52 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,8 +83,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/OtakuFlix_Network/4639')
+                    InlineKeyboardButton("🎀 ʟɪꜱᴛ ᴠɪᴘ", url='https://t.me/rajakonten_testi/83')
+                    InlineKeyboardButton('🎀 ᴛᴇꜱᴛɪᴍᴏɴɪ', url='https://t.me/rajakonten_testi')
                 ]
             ]
                 )
